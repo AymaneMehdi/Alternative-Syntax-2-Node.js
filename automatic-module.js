800c83f9dfc4dc7c6e6bf5fc41693331618c09c9
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-08-23 14:31:30 +0100

    automatic module

---
+++ automatic-module.js
@@ -0,0 +1,12 @@
+const num1 =5
+const num2 =10
+const num3 =4
+const num4 =4
+function addValues(){
+    console.log(`the sum is : ${num1+num2}`)
+}
+function addValues1(){
+    console.log(`the sum is : ${num3+num4}`)
+}
+addValues()
+addValues1()
